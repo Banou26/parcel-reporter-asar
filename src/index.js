3c4121a157162108b6182c31ef457d1e8db7af28
Banou <me@banou.dev>
2021-04-24 01:01:01 +0200

    fix: 🐛Parcel's API breaking change renaming

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { Reporter } from '@parcel/plugin'
 import asar from 'asar'
 
 export default new Reporter({
-  async report({ event: { type }, options: { outputFS, serve: { distDir } } }) {
+  async report({ event: { type }, options: { outputFS, serveOptions: { distDir } } }) {
     if (type !== 'buildSuccess') return
     await outputFS.rimraf(`${distDir}/package.asar`)
     await asar.createPackage(distDir, `${distDir}/package.asar`)
